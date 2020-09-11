af427632006f0a8a8e9a923ce0068c831012dcc8
Daniel Arrubla <darrubla@uninorte.edu.co>
2020-09-11 18:23:10 -0500

    changes

--- src/components/SideBar/index.js
+++ src/components/SideBar/index.js
@@ -1,48 +1,73 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import "./style/index.scss";
-import axios from 'axios';
+import axios from "axios";
 
 class index extends Component {
   constructor(props) {
     super(props);
-   // console.log(props)
+    // console.log(props)
     this.state = {
-       salary:'',
-    }
+      salary: "",
+    };
     // bind
-    
+
     this.handleChange = this.handleChange.bind(this);
     //this.search = this.search.bind(this);
-    
   }
 
   static propTypes = {
     prop: PropTypes,
   };
   handleChange({ target }) {
-    console.log(target.value)
-    let url = '/salary/'+target.value
-    axios.get(url)
-      .then(resp => {
-          console.log(resp.data);
+    console.log(target.value);
+    let url = "/salary/" + target.value;
+    axios
+      .get(url)
+      .then((resp) => {
+        console.log(resp.data);
 
-          this.setState({
-              jobs: resp.data,
-          });
-          
-          this.props.handleJobs(this.state.jobs);
+        this.setState({
+          jobs: resp.data,
+        });
+
+        this.props.handleJobs(this.state.jobs);
       })
-      .catch(err => {
-          console.log(err)
-      })
-  
+      .catch((err) => {
+        console.log(err);
+      });
   }
   render() {
     return (
       <div className="sidebar__container">
-        <h3>Salary Rank</h3>
-        <ul>
+        <form>
+          <div data-role="rangeslider">
+            <label for="range-1a">Salary Rank</label>
+            <input
+              type="range"
+              name="range-1a"
+              id="range-1a"
+              min="0"
+              max="130"
+              value="30"
+              // data-popup-enabled="true"
+              // data-show-value="true"
+            ></input>
+            <label for="range-1b">Salary Rank</label>
+            <input
+              type="range"
+              name="range-1b"
+              id="range-1b"
+              min="0"
+              max="130"
+              value="90"
+              // data-popup-enabled="true"
+              // data-show-value="true"
+            ></input>
+          </div>
+        </form>
+        {/* <h3>Salary Rank</h3>
+         <ul>
          
           <li>
             <input type="checkbox" name="salary" id="" value="25-40" onClick={this.handleChange}/> 25k - 40k
@@ -62,7 +87,11 @@ class index extends Component {
           <li>
             <input type="checkbox" name="salary" id="" value="120-180" onClick={this.handleChange}/>120k - 180k
           </li>
-        </ul>
+        </ul> */}
+        <figure>
+          <img src={image} alt="Graphiques" />
+          <figcaption>Graphiques</figcaption>
+        </figure>
       </div>
     );
   }
